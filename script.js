20bcf655c1cda77b54db40c8abbac3d0e8a764f3
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-27 18:05:36 -0800

    Added player and gameBoard factory functions.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+// TODO: create gaame, gameboard, and player objects
+
+// * Player factory function
+function createPlayer(name, marker) {
+  return { name, marker };
+}
+
+// * Gameboard module
+const gameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  const updateBoard = (index, marker) => {
+    board[index] = marker;
+  };
+
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+  };
+
+  return { getBoard, updateBoard, resetBoard };
+})();
+
+// * Game module
+
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+
+console.log(player1);
+console.log(player2);
+
+console.log(gameBoard.getBoard());
+gameBoard.updateBoard(0, player1.marker);
+console.log(gameBoard.getBoard());
+gameBoard.resetBoard();
+console.log(gameBoard.getBoard());
