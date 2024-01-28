54987305ed805edb2977b724a87e6ada69bb6e86
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-27 18:12:54 -0800

    Added isCellEmpty and isBoardFull methods to gameBoard factory function.

--- script.js
+++ script.js
@@ -19,11 +19,29 @@ const gameBoard = (() => {
     board = ["", "", "", "", "", "", "", "", ""];
   };
 
-  return { getBoard, updateBoard, resetBoard };
+  const isBoardfull = () => {
+    return board.every((cell) => cell !== "");
+  };
+
+  const isCellEmpty = (index) => {
+    return board[index] === "";
+  };
+
+  return { getBoard, updateBoard, resetBoard, isBoardfull, isCellEmpty };
 })();
 
 // * Game module
+const game = (() => {
+  // * Game logic
+  // TODO player turn
+  // TODO check win
+  // TODO check tie
+  // * Game flow
+})();
 
+// * Display controller module
+
+// * Test
 const player1 = createPlayer("Player 1", "X");
 const player2 = createPlayer("Player 2", "O");
 
@@ -35,3 +53,13 @@ gameBoard.updateBoard(0, player1.marker);
 console.log(gameBoard.getBoard());
 gameBoard.resetBoard();
 console.log(gameBoard.getBoard());
+
+for (let i = 0; i < 9; i++) {
+  gameBoard.updateBoard(i, player1.marker);
+}
+console.log(gameBoard.getBoard());
+console.log(gameBoard.isBoardfull());
+console.log(gameBoard.isCellEmpty(0));
+gameBoard.resetBoard();
+console.log(gameBoard.getBoard());
+console.log(gameBoard.isCellEmpty(0));
